7df0d807db7e1b468689cab64228ecca99198f66
Kireni <81343615+Kireni@users.noreply.github.com>
2023-11-22 12:11:34 +0100

    Add files via upload

---
+++ curr_conv.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from PIL import Image
+
+st.title("Currency Conversion")
+img = Image.open("conv.jpg")
+st.sidebar.image(img)
+
+curr = ["Naira", "Pounds", "Dollars", "Euros", "Yen", "Cedis"]
+conv = [1, 1023, 816, 890, 114, 68]
+
+def convert(num,initial,final):
+    ini_id = curr.index(initial)
+    fin_id = curr.index(final)
+    value1 = conv[ini_id]
+    value2 = conv[fin_id]
+    result = num*value1/value2
+
+    return round(result,2)
+
+num = st.number_input("How much are you converting")
+initial = st.selectbox("Amount Currency",curr)
+final = st.selectbox("Conversion Currency",curr)
+
+amount = convert(num,initial,final)
+
+if st.button("Convert"):
+    st.write(amount)
